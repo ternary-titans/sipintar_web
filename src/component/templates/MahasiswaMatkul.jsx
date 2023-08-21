049798b02d331707ec9b05a8590a12eeee9fe49e
anggunisme <rifkaanggun.08@gmail.com>
2023-08-22 02:28:27 +0700

    merge conflict

--- src/component/templates/MahasiswaMatkul.jsx
+++ src/component/templates/MahasiswaMatkul.jsx
@@ -32,8 +32,7 @@ export const MahasiswaMatkul = () => {
       ? JSON.parse(localStorage.getItem("userData")).token
       : null;
 
-    const mahasiswaId = localStorage.getItem("userData");
-    const idMhs = localStorage.getItem("userData")
+    const mahasiswaId = localStorage.getItem("userData")
       ? JSON.parse(localStorage.getItem("userData")).id
       : null;
 
