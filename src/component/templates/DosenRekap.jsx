1b4bd975d68823abd1d5b71416da9573075e526f
anggunisme <rifkaanggun.08@gmail.com>
2023-08-21 13:04:59 +0700

    Kurang Rekap Dosen

--- src/component/templates/DosenRekap.jsx
+++ src/component/templates/DosenRekap.jsx
@@ -1,45 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dosen from "./Dosen";
 import CardUser from "../atoms/CardUser";
 import Text from "../atoms/Text";
-import Input2 from "../atoms/InputDropdown";
 import Table from "../molecules/Tabel";
+import axios from "../../api/axios";
 
 export const DosenRekap = () => {
-  const [selectedOption, setSelectedOption] = useState("");
-
-  const handleDropdownChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
-
-  const dropdownOptions = [
-    { value: "option1", label: "Option 1" },
-    { value: "option2", label: "Option 2" },
-    { value: "option3", label: "Option 3" },
-  ];
+  const [showDetail, setShowDetail] = useState(-1);
   const columns = [
     "No",
-    "Kelas",
+    // "Kelas",
     "Mata Kuliah",
     "Total Jam Pertemuan",
-    "Presentase",
+    "Total Jam Kehadiran",
+    "Total Ketidakhadiran",
+    // "Presentase",
     "Aksi",
   ];
-  const data = [
-    {
-      No: 1,
-      Kelas: "IK3A",
-      "Mata Kuliah": "Pemr. Basis Data Jaringan",
-      "Total Jam Pertemuan": "32 jam",
-      Presentase: "100%",
-      Aksi: (
-        <button style={{ textDecoration: "underline", color: "blue" }}>
-          Detail
-        </button>
-      ),
-    },
-  ];
-
   const columnAlignments = [
     "center",
     "center",
@@ -56,6 +33,9 @@ export const DosenRekap = () => {
   const headerBorderColor2 = "#facc15";
   const pageSizeOptions = [5, 10, 25];
 
+  const [rekapDosenData, setRekapDosenData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const columns2 = [
     "No",
     "Tanggal Realisasi",
@@ -66,18 +46,48 @@ export const DosenRekap = () => {
     "Izin",
     "Alpa",
   ];
-  const data2 = [
-    {
-      No: 1,
-      "Tanggal Realisasi": "10 mei 2023",
-      "Jam Perkuliahan": "09.00 - 10.00",
-      Topik: "Topik 1",
-      Hadir: "1 mahasiswa",
-      Sakit: "1 mahasiswa",
-      Izin: "1 mahasiswa",
-      Alpa: "1 mahasiswa",
-    },
-  ];
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/dosen/1/rekapitulasiPresensi`
+        );
+        setRekapDosenData(response.data.data.rekapitulasi);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [rekapDosenData, setRekapDosenData]);
+
+  function formatDate(dateString) {
+    const dateObject = new Date(dateString);
+
+    const months = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+
+    const day = dateObject.getDate();
+    const month = dateObject.getMonth();
+    const year = dateObject.getFullYear();
+
+    const formattedDate = `${day} ${months[month]} ${year}`;
+    return formattedDate;
+  }
 
   return (
     <div>
@@ -86,24 +96,35 @@ export const DosenRekap = () => {
         <CardUser width={1220} borderColor="#9ca3af" borderWidth={2}>
           <div>
             <Text type="title3" text="Rekapitulasi Presensi Dosen" />
-            <div style={{ marginTop: "20px", width: "300px" }}>
-              <Input2
-                label="Tahun Ajaran"
-                value={selectedOption}
-                options={dropdownOptions}
-                onChange={handleDropdownChange}
-              />
-            </div>
             <div style={{ marginTop: "30px" }}>
-              <Table
-                columns={columns}
-                data={data}
-                columnAlignments={columnAlignments}
-                headerBackgroundColor={headerBackgroundColor}
-                headerBorderColor={headerBorderColor}
-                pageSizeOptions={pageSizeOptions}
-                style={{ marginTop: "10px" }}
-              />
+              {loading ? (
+                <p>Loading...</p>
+              ) : (
+                <Table
+                  columns={columns}
+                  data={rekapDosenData.map((item, index) => ({
+                    No: index + 1,
+                    "Mata Kuliah": item.mataKuliah,
+                    "Total Jam Pertemuan": item.total_jam,
+                    "Total Jam Kehadiran": item.total_hadir,
+                    "Total Ketidakhadiran": item.total_alpha,
+                    // Presentase: "50%",
+                    Aksi: (
+                      <button
+                        style={{ textDecoration: "underline", color: "blue" }}
+                        onClick={() => setShowDetail(index)}
+                      >
+                        Detail
+                      </button>
+                    ),
+                  }))}
+                  columnAlignments={columnAlignments}
+                  headerBackgroundColor={headerBackgroundColor}
+                  headerBorderColor={headerBorderColor}
+                  pageSizeOptions={pageSizeOptions}
+                  style={{ marginTop: "10px" }}
+                />
+              )}
             </div>
             <hr className="w-full h-0.5 bg-gray-300 mt-20 mb-2" />
             <Text
@@ -111,15 +132,39 @@ export const DosenRekap = () => {
               text="Rekapitulasi Presensi Dosen > {Kelas} > {MatKul}"
             />
             <div style={{ marginTop: "30px" }}>
-              <Table
-                columns={columns2}
-                data={data2}
-                columnAlignments={columnAlignments}
-                headerBackgroundColor={headerBackgroundColor}
-                headerBorderColor={headerBorderColor2}
-                pageSizeOptions={pageSizeOptions}
-                style={{ marginTop: "10px" }}
-              />
+              {showDetail !== -1 && (
+                <div style={{ marginTop: "20px" }}>
+                  <Text type="title3" text="Detail Rekapitulasi Presensi " />
+                  <div>
+                    {loading ? (
+                      <p>Loading...</p>
+                    ) : (
+                      <Table
+                        columns={columns2}
+                        data={rekapDosenData[showDetail].jadwalPertemuan.map(
+                          (item, index) => ({
+                            No: index + 1,
+                            "Tanggal Realisasi": formatDate(
+                              item.waktu_realisasi
+                            ),
+                            "Jam Perkuliahan": `${item.jam_mulai} - ${item.jam_akhir}`,
+                            Topik: item.topik_perkuliahan,
+                            Hadir: "",
+                            Sakit: "",
+                            Izin: "",
+                            Alpa: "",
+                          })
+                        )}
+                        columnAlignments={columnAlignments}
+                        headerBackgroundColor={headerBackgroundColor}
+                        headerBorderColor={headerBorderColor2}
+                        pageSizeOptions={pageSizeOptions}
+                        style={{ marginTop: "10px" }}
+                      />
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
         </CardUser>
